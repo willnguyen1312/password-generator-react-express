81ea98a7efb6c677a7bfcba7ec1d76b8e0ab6d6d
Nam Nguyen <nam.nguyen@huntermacdonald.com>
2017-08-15 12:43:53 +0700

    Cool!

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   // Initialize state
-  state = { passwords: [] }
+  state = { passwords: [], Hello: null };
 
   // Fetch passwords after first mount
   componentDidMount() {
@@ -12,50 +12,59 @@ class App extends Component {
 
   getPasswords = () => {
     // Get the passwords and store them in state
-    fetch('/api/passwords')
+    fetch("/api/passwords")
       .then(res => res.json())
       .then(passwords => this.setState({ passwords }));
-  }
+  };
 
+  getHope = () => {
+    let _Hello;
+    import("./NetworkError")
+      .then(({ default: NetworkError }) => {
+        console.log(NetworkError);
+        _Hello = NetworkError;
+        this.setState({
+          Hello: _Hello
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
   render() {
-    const { passwords } = this.state;
+    const { passwords, Hello } = this.state;
 
     return (
       <div className="App">
         {/* Render the passwords if we have them */}
-        {passwords.length ? (
-          <div>
-            <h1>5 Passwords.</h1>
-            <ul className="passwords">
-              {/*
+        {passwords.length
+          ? <div>
+              <h1>5 Passwords.</h1>
+              <ul className="passwords">
+                {/*
                 Generally it's bad to use "index" as a key.
                 It's ok for this example because there will always
                 be the same number of passwords, and they never
                 change positions in the array.
               */}
-              {passwords.map((password, index) =>
-                <li key={index}>
-                  {password}
-                </li>
-              )}
-            </ul>
-            <button
-              className="more"
-              onClick={this.getPasswords}>
-              Get More
-            </button>
-          </div>
-        ) : (
-          // Render a helpful message otherwise
-          <div>
-            <h1>No passwords :(</h1>
-            <button
-              className="more"
-              onClick={this.getPasswords}>
-              Try Again?
-            </button>
-          </div>
-        )}
+                {passwords.map((password, index) =>
+                  <li key={index}>
+                    {password}
+                  </li>
+                )}
+              </ul>
+              <button className="more" onClick={this.getPasswords}>
+                Get More
+              </button>
+            </div>
+          : // Render a helpful message otherwise
+            <div>
+              <h1>No passwords :(</h1>
+              <button className="more" onClick={this.getHope}>
+                Try Again?
+              </button>
+            </div>}
+        {Hello && <Hello />}
       </div>
     );
   }
---
+++ client/src/NetworkError.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NetworkError = () =>
+  <div>
+    <h1>Awww - No hope :(</h1>
+  </div>;
+export default NetworkError;
